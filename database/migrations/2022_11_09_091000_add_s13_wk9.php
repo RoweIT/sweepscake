7037198bb6c31b6786fcfb12677d1c702c4b5b86
Paul Illingworth <paul.illingworth@roweit.co.uk>
2022-11-16 09:03:54 +0000

    Add week 10 scores

--- database/migrations/2022_11_09_091000_add_s13_wk9.php
+++ database/migrations/2022_11_09_091000_add_s13_wk9.php
@@ -20,16 +20,16 @@ return new class extends Migration {
         $syabira = Baker::findBySlug('syabira-13');
         $sandro = Baker::findBySlug('sandro-13');
 
-        $s13_wk8 = Week::create(['series_id' => $series13->id,
+        $s13_wk9 = Week::create(['series_id' => $series13->id,
             'week_num' => 9,
             'theme' => 'Pâtisserie', 'signature' => 'Mini Charlottes', 'technical' => 'Chocolate, Hazelnut and Raspberry Vertical Tarts', 'showstopper' => 'Krokan']);
 
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $abdul->id, 'type' => EVENT::TYPE_STAR_BAKER]);
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $syabira->id, 'type' => EVENT::TYPE_TECHNICAL_FIRST]);
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $abdul->id, 'type' => EVENT::TYPE_TECHNICAL_SECOND]);
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $janusz->id, 'type' => EVENT::TYPE_TECHNICAL_THIRD]);
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $sandro->id, 'type' => EVENT::TYPE_TECHNICAL_LAST]);
-        Event::create(['week_id' => $s13_wk8->id, 'baker_id' => $janusz->id, 'type' => EVENT::TYPE_ELIMINATED]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $abdul->id, 'type' => EVENT::TYPE_STAR_BAKER]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $syabira->id, 'type' => EVENT::TYPE_TECHNICAL_FIRST]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $abdul->id, 'type' => EVENT::TYPE_TECHNICAL_SECOND]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $janusz->id, 'type' => EVENT::TYPE_TECHNICAL_THIRD]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $sandro->id, 'type' => EVENT::TYPE_TECHNICAL_LAST]);
+        Event::create(['week_id' => $s13_wk9->id, 'baker_id' => $janusz->id, 'type' => EVENT::TYPE_ELIMINATED]);
     }
 
     /**
